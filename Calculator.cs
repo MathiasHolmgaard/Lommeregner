168b9236ba61f1b3807de564b5ec2d73d7abb196
Mathias Holmgaard <mathias.holmgaard@hotmail.com>
2023-02-18 23:17:34 +0100

    Test

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner
+
+{
+        /* Hele denne class skal rettes til
+
+    internal class Calculator
+    {
+        public static int Add(int x, int y)
+        { 
+            return x + y; }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static double Divide(int x, int y)
+        {
+            return x / y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+        */
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Lommeregner
+﻿using System.ComponentModel;
+
+namespace Lommeregner
 {
     internal class Program
+        /* Jeg skal finde ud af hvordan jeg kalder metoder fra klassen Calculator*/
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            {
+                while (true)
+                {
+                    double num1, num2;
+                    char op;
+
+                    Console.Write("Indtast første tal: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Indsæt tegn: ");
+                    op = Convert.ToChar(Console.ReadLine());
+
+                    Console.Write("Indtast næste tal: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+
+                    switch (op)
+                    {
+                        case '+':
+                            Console.WriteLine(num1 + num2);
+                            break;
+                        case '-':
+                            Console.WriteLine(num1 - num2);
+                            break;
+                        case '*':
+                            Console.WriteLine(num1 * num2);
+                            break;
+                        case '/':
+                            Console.WriteLine(num1 / num2);
+                            break;
+                        default:
+                            Console.WriteLine("Ugyldigt tegn!");
+                            break;
+                    }
+                    Console.WriteLine("Tryk ENTER for at prøve igen");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
